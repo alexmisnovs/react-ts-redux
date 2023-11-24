00799234af3406fd6b0dbf50ca6f360cfe6ff3f6
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-11-24 15:49:37 +0000

    added store, cartSlice, setup provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import Header from './components/Header.tsx';
-import Shop from './components/Shop.tsx';
-import Product from './components/Product.tsx';
-import { DUMMY_PRODUCTS } from './dummy-products.ts';
+import Header from "./components/Header.tsx";
+import Shop from "./components/Shop.tsx";
+import Product from "./components/Product.tsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.ts";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <Shop>
-        {DUMMY_PRODUCTS.map((product) => (
+        {DUMMY_PRODUCTS.map(product => (
           <li key={product.id}>
             <Product {...product} />
           </li>
         ))}
       </Shop>
-    </>
+    </Provider>
   );
 }
 
